d1d972a9f719a9f09c59fc4c59dd7b16b5f731ed
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-05-29 14:11:17 -0500

    minor

--- index.test.js
+++ index.test.js
@@ -2,9 +2,9 @@ import { sum } from "./";
 
 describe("sum tests", () => {
   test("sum returns sum correctly", () => {
-    const expected = 8;
+    const expected = 7;
 
-    const actual = sum(3, 5);
+    const actual = sum(3, 4);
 
     expect(actual).toBe(expected);
   });
